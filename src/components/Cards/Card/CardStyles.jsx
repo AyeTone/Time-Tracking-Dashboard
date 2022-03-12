8f13ab8e9a8350d9a2e85df709922cb9e8f95a43
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-12 12:15:18 -0500

    Card Styles Complete

--- src/components/Cards/Card/CardStyles.jsx
+++ src/components/Cards/Card/CardStyles.jsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+
   width: 100%;
   background-color: ${(props) => props.bg};
   padding-top: 3.5rem;
@@ -8,7 +12,14 @@ export const Container = styled.div`
   border-radius: var(--borderRad);
 `;
 
-export const BgImage = styled.img``;
+export const BgImage = styled.img`
+  z-index: -1;
+  position: absolute;
+  top: -15px;
+  right: 10px;
+
+  width: 100px;
+`;
 
 export const CardInfo = styled.div`
   width: 100%;
@@ -32,22 +43,34 @@ export const Heading = styled.div`
   align-items: center;
 `;
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  font-size: 1.3rem;
+`;
 
 export const Dots = styled.img`
   width: 8%;
 
-  opacity: 0.7;
+  opacity: 0.8;
   transition: 0.25s ease-in-out;
 
   &:hover {
     opacity: 1;
-    filter: brightness(20);
   }
 `;
 
-export const Times = styled.div``;
+export const Times = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-export const CurrentTime = styled.p``;
+  margin-top: 1rem;
+`;
 
-export const PrevTime = styled.p``;
+export const CurrentTime = styled.p`
+  font-size: 1.9rem;
+  font-weight: lighter;
+`;
+
+export const PrevTime = styled.p`
+  color: var(--CardHover);
+`;
